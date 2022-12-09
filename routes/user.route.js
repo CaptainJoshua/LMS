501046100fd13136913deed34d5d5b27c1e02d9e
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-12-10 03:23:41 +0800

    added the route for the subjects

--- routes/user.route.js
+++ routes/user.route.js
@@ -11,4 +11,24 @@ router.get('/dashboard', async(req, res, next) => {
     res.render('dashboard', { person });
 });
 
+router.get('/dashboard/english', async(req, res, next) => {
+    const person = req.user;
+    res.render('english', { person });
+});
+
+router.get('/dashboard/math', async(req, res, next) => {
+    const person = req.user;
+    res.render('math', { person });
+});
+
+router.get('/dashboard/filipino', async(req, res, next) => {
+    const person = req.user;
+    res.render('filipino', { person });
+});
+
+router.get('/dashboard/readPhonics', async(req, res, next) => {
+    const person = req.user;
+    res.render('readPhonics', { person });
+});
+
 module.exports = router;
